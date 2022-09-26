fb3af8320f3e03de4e0cf0da0248ad984cc71779
tina_tt2 <tina.t2aq@gmail.com>
2022-09-26 17:18:23 +0330

    Finish SignalingClient class

--- app/src/main/java/com/example/videocall/SignalingClient.kt
+++ app/src/main/java/com/example/videocall/SignalingClient.kt
@@ -4,10 +4,8 @@ import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import org.webrtc.IceCandidate
 import org.webrtc.SessionDescription
 import kotlin.coroutines.CoroutineContext
 
@@ -25,6 +23,8 @@ class SignalingClient(
         const val SDP_CANDIDATE = "sdpCandidate"
         const val SDP_MID = "sdpMid"
         const val SDP_LINE_INDEX = "sdpMLineIndex"
+        const val TYPE_OFFER_CANDIDATE = "offerCandidate"
+        const val TYPE_ANSWER_CANDIDATE = "answerCandidate"
 
     }
 
@@ -37,6 +37,32 @@ class SignalingClient(
 
     val network = Firebase.firestore
     var sdpType: String? = null
+
+
+    fun sendCandidate(candidate: IceCandidate?, isJoin: Boolean) = runBlocking {
+        val type = when {
+            isJoin -> TYPE_ANSWER_CANDIDATE
+            else -> TYPE_OFFER_CANDIDATE
+        }
+        val candidateConstant = hashMapOf(
+            "serverUrl" to candidate?.serverUrl,
+            "sdpMid" to candidate?.sdpMid,
+            "sdpMLineIndex" to candidate?.sdpMLineIndex,
+            "sdpCandidate" to candidate?.sdp,
+            "type" to type
+        )
+
+        network.collection(CollectionEnum.CALLS.value)
+            .document(meetingID)
+            .collection(CollectionEnum.CANDIDATES.value)
+            .document(type)
+            .set(candidateConstant)
+            .addOnSuccessListener {
+                Log.e(TAG, "sendIceCandidate: Success")
+            }.addOnFailureListener {
+                Log.e(TAG, "sendIceCandidate: Error $it")
+            }
+    }
 
     private fun connect() = launch {
         network.enableNetwork().addOnCanceledListener {
@@ -51,24 +77,16 @@ class SignalingClient(
         }
     }
 
-    private fun candidateAddedToTheCallObserver() {
-        network.collection(CollectionEnum.CALLS.value)
-            .document(meetingID)
-            .collection(CollectionEnum.CANDIDATES.value)
-            .addSnapshotListener { querysnapshot, error ->
-                if (checkError(error)) return@addSnapshotListener
-
-                //                        condidateAddedToTheCall(data)
-            }
-    }
-
     private fun offerAnswerEndObserver() {
         network.collection(CollectionEnum.CALLS.value)
             .document(meetingID)
             .addSnapshotListener { snapshot, error ->
                 if (checkError(error)) return@addSnapshotListener
                 val data = snapshot?.data
-                if (snapshot != null && snapshot.exists() && data?.containsKey(KEY_TYPE) != null) {
+                if (snapshot != null && snapshot.exists() && data?.containsKey(KEY_TYPE) != null && data.containsKey(
+                        KEY_TYPE
+                    )
+                ) {
                     offerAnswerEnd(data)
                 }
             }
@@ -111,12 +129,53 @@ class SignalingClient(
                 }
             }
         }
-        Log.d(TAG,"snapshot data:$data")
+        Log.d(TAG, "snapshot data:$data")
     }
 
-    private fun candidateAddedToTheCall() {
-        TODO("Not yet implemented")
+
+    private fun candidateAddedToTheCallObserver() {
+        network.collection(CollectionEnum.CALLS.value)
+            .document(meetingID)
+            .collection(CollectionEnum.CANDIDATES.value)
+            .addSnapshotListener { querysnapshot, error ->
+                if (checkError(error)) return@addSnapshotListener
+                if (querysnapshot != null && !querysnapshot.isEmpty) {
+                    for (dataSnapshot in querysnapshot) {
+                        val data = dataSnapshot.data
+                        val type = data.getValue(KEY_TYPE).toString()
+                        if (dataSnapshot != null && dataSnapshot.exists() && data.containsKey(
+                                KEY_TYPE
+                            )
+                        ) candidateAddedToTheCall(type, data)
+
+                        Log.e(TAG, "candidateQuery: $dataSnapshot")
+                    }
+                }
+            }
     }
+
+    private fun candidateAddedToTheCall(
+        type: String,
+        data: Map<String, Any>
+    ) {
+        when {
+            sdpType == SDPType.OFFER.value && type == TYPE_OFFER_CANDIDATE -> {
+                listener.onIceCandidateReceived(
+                    fillIceCandidate(data)
+                )
+            }
+            sdpType == SDPType.ANSWER.value && type == TYPE_ANSWER_CANDIDATE -> {
+                fillIceCandidate(data)
+            }
+        }
+    }
+
+    private fun fillIceCandidate(data: Map<String, Any>) =
+        IceCandidate(
+            data[SDP_MID].toString(), Math.toIntExact(
+                data[SDP_LINE_INDEX] as Long
+            ), data[SDP_CANDIDATE].toString()
+        )
 
     fun destroy() {
         job.complete()
