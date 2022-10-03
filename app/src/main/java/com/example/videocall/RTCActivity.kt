b87c503a60155e68ed704501390de5f90282f996
tina_tt2 <tina.t2aq@gmail.com>
2022-10-03 17:55:46 +0330

    Fix not opening RTCActivity

--- app/src/main/java/com/example/videocall/RTCActivity.kt
+++ app/src/main/java/com/example/videocall/RTCActivity.kt
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isGone
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.webrtc.IceCandidate
 import org.webrtc.MediaStream
 import org.webrtc.SessionDescription
@@ -56,8 +57,8 @@ class RTCActivity : AppCompatActivity() {
 
     private val sdpObserver = object : AppSdpObserver() {}
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rtc)
         initUiViews()
         initUiComponent()
